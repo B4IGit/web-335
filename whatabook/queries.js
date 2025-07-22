5d60d3ef7936305cedb3cd1c5b6e450bf274b447
Devin Ledesma <dev.dfledesma@gmail.com>
2025-07-22 13:17:57 -0500

    added code commits for title, author, date, and ddescription; added load script to connect to script.js

--- whatabook/queries.js
+++ whatabook/queries.js
@@ -1,16 +1,26 @@
+/*
+Title: queries.js
+Author: Devin Ledesma, Riley Bird
+Date: 07/18/2025
+Description: MongoDB shell scripts for WhatABook.
+*/
+
+// Load script.js
+load('script.js')
+
 //Query to display a list of books
-db.books.find()
+db.books.find();
 
 //Query to display a list of books by genre
 db.books.aggregate([
   { $group: { _id: "$genre", books: { $push: "$title" } } }
-])
+]);
 
 //Query to display a list of books by author
 db.books.aggregate([
   { $group: { _id: "$author", books: { $push: "$title" } } }
-])
+]);
 
 //Query to display a book by bookId
-db.books.findOne({ bookId: "b101" })
+db.books.findOne({ bookId: "b101" });
 
